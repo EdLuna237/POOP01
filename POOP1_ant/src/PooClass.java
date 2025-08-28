3149524f49c412810e03125b98cfec97bb53d300
EdLuna237 <edgar.lusan237@gmail.com>
2025-08-27 23:07:24 -0600

    Add files via upload

---
+++ POOP1_ant/src/PooClass.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author poo02alu10
+ */
+public class PooClass {
+    
+    public static void main(String[] args){
+        System.out.println("Hola Mundo!");
+        
+        int a = 3;
+        float b = 3.5f;
+        float c = a+b;
+        System.out.println(c);
+        float e= 7.5f;
+        System.out.println(e);
+        
+    }
+    
+}
